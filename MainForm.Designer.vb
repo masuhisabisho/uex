574d4f61087479d75c7df184c14355b247ba1c27
masuhisabisho <madman190382@yahoo.co.jp>
2013-06-14 23:24:50 +0900

    real commit 0001

---
+++ MainForm.Designer.vb
@@ -0,0 +1,98 @@
+﻿'
+' Created by SharpDevelop.
+' User: madman190382
+' Date: 2013/06/14
+' Time: 23:08
+' 
+' To change this template use Tools | Options | Coding | Edit Standard Headers.
+'
+Partial Class MainForm
+	Inherits System.Windows.Forms.Form
+	
+	''' <summary>
+	''' Designer variable used to keep track of non-visual components.
+	''' </summary>
+	Private components As System.ComponentModel.IContainer
+	
+	''' <summary>
+	''' Disposes resources used by the form.
+	''' </summary>
+	''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		If disposing Then
+			If components IsNot Nothing Then
+				components.Dispose()
+			End If
+		End If
+		MyBase.Dispose(disposing)
+	End Sub
+	
+	''' <summary>
+	''' This method is required for Windows Forms designer support.
+	''' Do not change the method contents inside the source code editor. The Forms designer might
+	''' not be able to load this method if it was changed manually.
+	''' </summary>
+	Private Sub InitializeComponent()
+		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+		Me.button1 = New System.Windows.Forms.Button()
+		Me.button2 = New System.Windows.Forms.Button()
+		Me.button3 = New System.Windows.Forms.Button()
+		Me.button4 = New System.Windows.Forms.Button()
+		Me.SuspendLayout
+		'
+		'button1
+		'
+		Me.button1.Location = New System.Drawing.Point(209, 60)
+		Me.button1.Name = "button1"
+		Me.button1.Size = New System.Drawing.Size(75, 23)
+		Me.button1.TabIndex = 0
+		Me.button1.Text = "button1"
+		Me.button1.UseVisualStyleBackColor = true
+		'
+		'button2
+		'
+		Me.button2.Location = New System.Drawing.Point(209, 114)
+		Me.button2.Name = "button2"
+		Me.button2.Size = New System.Drawing.Size(75, 23)
+		Me.button2.TabIndex = 1
+		Me.button2.Text = "button2"
+		Me.button2.UseVisualStyleBackColor = true
+		'
+		'button3
+		'
+		Me.button3.Location = New System.Drawing.Point(209, 164)
+		Me.button3.Name = "button3"
+		Me.button3.Size = New System.Drawing.Size(75, 23)
+		Me.button3.TabIndex = 2
+		Me.button3.Text = "button3"
+		Me.button3.UseVisualStyleBackColor = true
+		'
+		'button4
+		'
+		Me.button4.Location = New System.Drawing.Point(209, 213)
+		Me.button4.Name = "button4"
+		Me.button4.Size = New System.Drawing.Size(75, 23)
+		Me.button4.TabIndex = 3
+		Me.button4.Text = "button4"
+		Me.button4.UseVisualStyleBackColor = true
+		'
+		'MainForm
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 12!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"),System.Drawing.Image)
+		Me.ClientSize = New System.Drawing.Size(392, 293)
+		Me.Controls.Add(Me.button4)
+		Me.Controls.Add(Me.button3)
+		Me.Controls.Add(Me.button2)
+		Me.Controls.Add(Me.button1)
+		Me.Icon = CType(resources.GetObject("$this.Icon"),System.Drawing.Icon)
+		Me.Name = "MainForm"
+		Me.Text = "uex"
+		Me.ResumeLayout(false)
+	End Sub
+	Private button4 As System.Windows.Forms.Button
+	Private button3 As System.Windows.Forms.Button
+	Private button2 As System.Windows.Forms.Button
+	Private button1 As System.Windows.Forms.Button
+End Class
---
+++ MainForm.vb
@@ -0,0 +1,18 @@
+﻿'
+' Created by SharpDevelop.
+' User: madman190382
+' Date: 2013/06/14
+' Time: 23:08
+' 
+' To change this template use Tools | Options | Coding | Edit Standard Headers.
+'
+Public Partial Class MainForm
+	Public Sub New()
+		' The Me.InitializeComponent call is required for Windows Forms designer support.
+		Me.InitializeComponent()
+		
+		'
+		' TODO : Add constructor code after InitializeComponents
+		'
+	End Sub
+End Class
